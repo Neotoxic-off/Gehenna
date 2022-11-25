f85639c759978e66abe0dc3cbec1b9849727f13a
Neotoxic-off <neotoxic.off@gmail.com>
2022-11-25 16:40:36 +0100

    🌱 FEATURE | update login page

--- Gehenna/app/src/index.js
+++ Gehenna/app/src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
+import Login from "./pages/Login";
 import Layout from "./pages/Layout";
 
 import './App.css'
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>

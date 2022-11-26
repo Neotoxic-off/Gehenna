fe4c347cdc8c56aa2f228a25d9e8f5e82730b0b6
neo <neotoxic.off@gmail.com>
2022-11-26 01:03:11 +0100

    add links to the cheats

--- Gehenna/app/src/index.js
+++ Gehenna/app/src/index.js
@@ -1,8 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Error from "./pages/Error";
-import Login from "./pages/Login";
 import Layout from "./pages/Layout";
 
 import './App.css'
@@ -13,8 +11,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- Gehenna/app/src/pages/Home.js
+++ Gehenna/app/src/pages/Home.js
@@ -1,19 +1,185 @@
-import Button from "../components/Button";
-import Header from "../components/Header";
+import "../library/styles/Root.css"
+import "../library/styles/Grid.css"
+import "../library/styles/Panel.css"
 
-import "../styles/grid.css"
-
-// import { ReactComponent as Github } from "../assets/icons/svg/github.svg";
+import Article from "../library/components/Article";
+import Button from "../library/components/Button";
 
 function Home() {
   return (
     <>
-      {/* <Header profile="https://avatars.githubusercontent.com/u/44700383?v=4"/> */}
-      <div className="container">
-        <div className="item">
-          <Button
-            text="Login"
-            link="https://github.com/Neotoxic-off"
+      <div className="grid column panel">
+        <div className="item article">
+          <Article title="Status"
+            typetitle="title text-left"
+          />
+          <div className="item panel">
+            <Article description="All platforms"
+              typedescription="description text-left"
+            />
+            <p className="body text-left">
+              🔴 Cookie Patcher<br/>
+              🟣 Dies Irae<br/>
+              🟣 Immunity<br/>
+              🔴 Stabbed<br/>
+              🟣 Kyrie<br/>
+            </p>
+          </div>
+          <div className="item panel">
+            <Article description="Steam"
+              typedescription="description text-left"
+            />
+            <p className="body text-left">
+              🔴 Anakata<br/>
+            </p>
+          </div>
+          <div className="item panel">
+            <Article description="Epic Games"
+              typedescription="description text-left"
+            />
+            <p className="body text-left">
+              🟢 Anakata<br/>
+            </p>
+          </div>
+          <div className="item panel">
+            <Article description="Microsoft"
+              typedescription="description text-left"
+            />
+            <p className="body text-left">
+              🟢 Anakata<br/>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="grid column panel">
+        <div className="item article">
+          <Article title="Anakata"
+            description="Unlock characters, skins & charms"
+            body="Anakata is an unlocker who can give you all the skins, characters and chams temporarily"
+            footer="v1.0"
+
+            typetitle="title text-left"
+            typedescription="description text-left"
+            typebody="body text-left"
+            typefooter="footer text-left"
+          />
+          <Button type="button contained item-left"
+            link="https://github.com/Elevatia/panel/releases/download/1.1.0/Ana.Kata.zip"
+            text="Download"
+          />
+          <Button type="button filled item-left margin-left"
+            link="https://github.com/Elevatia/anakata"
+            text="Source code"
+          />
+        </div>
+      </div>
+      <div className="grid column panel">
+        <div className="item article">
+          <Article title="Cookie Patcher"
+            description="Bypass packchunk signature file to get your cookie"
+            body="Automatic offset finder injecting bytes in packchunk file to enable peer verification"
+            footer="v1.0"
+
+            typetitle="title text-left"
+            typedescription="description text-left"
+            typebody="body text-left"
+            typefooter="footer text-left"
+          />
+          <Button type="button contained item-left"
+            link="https://github.com/Elevatia/panel/releases/download/1.1.0/Cookie.Patcher.zip"
+            text="Download"
+          />
+          <Button type="button filled item-left margin-left"
+            link="https://github.com/gehennaclub/Aurora"
+            text="Source code"
+          />
+        </div>
+      </div>
+      <div className="grid column panel">
+        <div className="item article">
+          <Article title="Dies Irae"
+            description="String extractor from binaries for datamining"
+            body="Extract strings from any file using patern matching and ascii validation"
+            footer="v1.0"
+
+            typetitle="title text-left"
+            typedescription="description text-left"
+            typebody="body text-left"
+            typefooter="footer text-left"
+          />
+          <Button type="button contained item-left"
+            link="https://github.com/Elevatia/panel/releases/download/1.1.0/Dies.Irae.zip"
+            text="Download"
+          />
+          <Button type="button filled item-left margin-left"
+            link="https://github.com/Elevatia/Dies-Irae"
+            text="Source code"
+          />
+        </div>
+      </div>
+      <div className="grid column panel">
+        <div className="item article">
+          <Article title="Kyrie"
+            description="Heavy client to download cheats"
+            body="Client to download all my cheat collection with the latest updates"
+            footer="v1.0"
+
+            typetitle="title text-left"
+            typedescription="description text-left"
+            typebody="body text-left"
+            typefooter="footer text-left"
+          />
+          <Button type="button contained item-left"
+            link="https://github.com/Elevatia/panel/releases/download/1.1.0/Kyrie.zip"
+            text="Download"
+          />
+          <Button type="button filled item-left margin-left"
+            link="https://github.com/Elevatia/Kyrie"
+            text="Source code"
+          />
+        </div>
+      </div>
+      <div className="grid column panel">
+        <div className="item article">
+          <Article title="Immunity"
+            description="Unreal Engine configuration builder"
+            body="Build own custom engine.ini file configuration"
+            footer="v1.0"
+
+            typetitle="title text-left"
+            typedescription="description text-left"
+            typebody="body text-left"
+            typefooter="footer text-left"
+          />
+          <Button type="button contained item-left"
+            link="https://github.com/Elevatia/panel/releases/download/1.1.0/Immunity.zip"
+            text="Download"
+          />
+          <Button type="button filled item-left margin-left"
+            link="https://github.com/Elevatia/Immunity"
+            text="Source code"
+          />
+        </div>
+      </div>
+      <div className="grid column panel">
+        <div className="item article">
+          <Article title="Stabbed"
+            description="Unreal Engine configurations patcher"
+            body="Patch your game using custom configuration"
+            footer="v1.0"
+
+            typetitle="title text-left"
+            typedescription="description text-left"
+            typebody="body text-left"
+            typefooter="footer text-left"
+          />
+          <Button type="button contained item-left"
+            link="https://github.com/Elevatia/panel/releases/download/1.1.0/Stabbed.zip"
+            text="Download"
+          />
+          <Button type="button filled item-left margin-left"
+            link="https://github.com/Elevatia/Stabbed"
+            text="Source code"
           />
         </div>
       </div>
